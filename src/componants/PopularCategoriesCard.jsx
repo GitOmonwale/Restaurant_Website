02cb9542edf2eac524f510266d11729b5445b9dc
GitOmonwalé <inesagbozo@gmail>
2024-09-07 21:39:22 +0100

    modification

--- src/componants/PopularCategoriesCard.jsx
+++ src/componants/PopularCategoriesCard.jsx
@@ -5,7 +5,7 @@ const PopularCategoriesCard = ({ data }) => {
         <>
             {
                 data.map(item => (
-                    <div className='rounded-3xl shadow-xl py-10 px-5 flex flex-col items-center gap-2' key={item.id}>
+                    <div className='rounded-3xl shadow-md py-10 px-5 flex flex-col items-center gap-2' key={item.id}>
                         <div className='bg-turquois rounded-full flex items-center justify-center sm:h-32 sm:w-32 h-24 w-24'>
                             <img src={item.image} alt="" />
                         </div>
