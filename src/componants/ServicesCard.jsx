70ce38e8d25aa5c17cfe7c37149283de739e6a7c
GitOmonwalé <inesagbozo@gmail>
2024-09-07 21:57:19 +0100

    modification

--- src/componants/ServicesCard.jsx
+++ src/componants/ServicesCard.jsx
@@ -5,7 +5,7 @@ const ServicesCard = ({ data }) => {
         <>
             {
                 data.map(item => (
-                    <div className='rounded-3xl shadow-sm shadow-gray md:py-10 py-3 md:px-5 px-3 flex flex-col items-center gap-2 text-center' key={item.id}>
+                    <div className='rounded-3xl shadow-md shadow-gray md:py-10 py-3 md:px-5 px-3 flex flex-col items-center gap-2 text-center' key={item.id}>
                         <img src={item.image} alt="" />
                         <div className='font-bold text-green uppercase'>{item.title}</div>
                         <div className='text-[#90BD95]'>{item.description}</div>
